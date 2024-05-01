eb206cefa180d6aea1cf7dfb2252e039ccecea02
Jorge Bau Gracia <jbaugracia@gmail.com>
2024-05-01 21:03:35 +0200

    Upgraded java version to 21, using lombok modern java features where possible

--- src/main/java/minesweeper/gui/Main.java
+++ src/main/java/minesweeper/gui/Main.java
@@ -23,8 +23,8 @@ public class Main {
 			e.printStackTrace();
 		}
 		// Main window, with a grid bag layout
-		GridBagLayout gridBagLayout1 = new GridBagLayout();
-		JFrame mainWindow = new JFrame();
+		var gridBagLayout1 = new GridBagLayout();
+		var mainWindow = new JFrame();
 		mainWindow.getContentPane().setLayout(gridBagLayout1);
 		// Aspect and behavior of the main window
 		mainWindow.setLocation(200, 200);
@@ -32,13 +32,14 @@ public class Main {
 		mainWindow.setSize(570, 500);
 		mainWindow.setTitle("Minesweeper");
 		// Create a game panel
-		GamePanel pj = new GamePanel();
+		var pj = new GamePanel();
 		pj.setBorder(BorderFactory.createLineBorder(Color.WHITE));
 		mainWindow.getContentPane().add(
 				pj,
 				new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0,
 						GridBagConstraints.CENTER, GridBagConstraints.BOTH,
-						new Insets(10, 10, 10, 10), 0, 0));
+						new Insets(10, 10, 10, 10), 0, 0)
+		);
 		// Show the panel (in a centered position)
 		mainWindow.setLocationRelativeTo(null);
 		mainWindow.setVisible(true);
